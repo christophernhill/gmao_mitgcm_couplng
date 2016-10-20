e0dd1e2a5114c0816fe1a35370fb0c5162e1b8d6
menemenlis <dmenemenlis@gmail.com>
2016-10-20 11:27:27 -0700

    Moving environment set-up actions from notes to set_mitgcm_env.sh

--- set_mitgcm_env.sh
+++ set_mitgcm_env.sh
@@ -21,3 +21,22 @@ cd mitgcm_setup
 tar -xf mitgcm_special_bits.tgz
 echo "Unpacked mitgcm_special_bits.tgz"
 )
+
+echo "Applied Atanas' changes to MITgcm"
+cd $GMAO_MITGCM_COUPLNG/modifications
+cp timers.F $WorkingDir/MITgcm/eesupp/src
+cd ../mitgcm_setup/build
+ln -sf /usr/lib/openmpi/include/mpif.h .
+
+echo "Linked code from subdirectories to code_split_driver"
+cd $GMAO_MITGCM_COUPLNG/mitgcm_setup/code_split_driver
+ln -sf driver/*.F* .
+ln -sf state/dynvars_h/* .
+ln -sf state/export/* .
+ln -sf state/ffields_h/*.F90 .
+ln -sf state/ffields_h/*.h .
+ln -sf state/import/* .
+ln -sf state/mitgcm_state/* .
+ln -sf state/stackvars/* .
+ln -sf state/timevars/* .
+ln -sf utils/*.F90 .
