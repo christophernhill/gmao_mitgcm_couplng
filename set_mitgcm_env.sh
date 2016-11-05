6b1591832488606997116c15f949bfd3a924b5d6
menemenlis <dmenemenlis@gmail.com>
2016-11-04 22:45:59 -0700

    Updating comments for consistency

--- set_mitgcm_env.sh
+++ set_mitgcm_env.sh
@@ -4,7 +4,7 @@ if [ "x${MITGCM_ROOT}" == "x" ]; then
  exit
 fi
 
-# unpack Makefile and inc/*h files 
+# Unpack Makefile and inc/*h files 
 # **** this needs to be cleaned up                         ****
 # **** the files should be generated from a script somehow ****
 (
@@ -13,6 +13,11 @@ tar -xf mitgcm_special_bits.tgz
 echo "Unpacked mitgcm_special_bits.tgz"
 )
 
+# Move Fortran files from subdirectories in mitgcm_setup/code_split_driver
+# to a flat set of files in the code_split_driver directory.
+# Original instructions are from:
+# https://github.com/christophernhill/gmao_mitgcm_couplng/blob/master/chris_notes/dimitris_notes.md
+# using: find driver/ utils/ state/ -type f | awk '{print "cp "$1" ."}'
 echo "Linked code from subdirectories to code_split_driver"
 cd $GMAO_MITGCM_COUPLNG/mitgcm_setup/code_split_driver
 ln -sf driver/*.F* .
