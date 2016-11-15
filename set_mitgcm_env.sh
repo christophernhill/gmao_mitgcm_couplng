e9dcc6d269c891e89c61427eeced919dae550701
menemenlis <dmenemenlis@gmail.com>
2016-11-14 16:10:58 -0800

    Making flat directory structure by calling mk_src_links

--- set_mitgcm_env.sh
+++ set_mitgcm_env.sh
@@ -11,13 +11,5 @@ fi
 # using: find driver/ utils/ state/ -type f | awk '{print "cp "$1" ."}'
 echo "Linked code from subdirectories to code_split_driver"
 cd $GMAO_MITGCM_COUPLNG/mitgcm_setup/code_split_driver
-ln -sf driver/*.F* .
-ln -sf state/dynvars_h/* .
-ln -sf state/export/* .
-ln -sf state/ffields_h/*.F90 .
-ln -sf state/ffields_h/*.h .
-ln -sf state/import/* .
-ln -sf state/mitgcm_state/* .
-ln -sf state/stackvars/* .
-ln -sf state/timevars/* .
-ln -sf utils/*.F90 .
+./mk_src_links
+
