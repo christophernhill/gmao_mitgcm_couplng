68b3a09f30934f03cf251e799a399e5f58b9b001
menemenlis <dmenemenlis@gmail.com>
2016-11-14 20:34:46 -0800

    matlab scripts for reading time series

---
+++ matlab/get_GEOS5.m
@@ -0,0 +1,22 @@
+% Get GEOS-5 output time series with matlab.
+
+% Replace with location of your output file
+pnm='/Users/dmenemen/Desktop/scratch/';
+
+% Read and plot mit_ocn variables
+fnm=dir([pnm 'TEST.mit*']);
+nx=48;
+ny=25;
+nt=length(fnm);
+lat=ncread(fnm(1).name,'lat');
+lon=ncread(fnm(1).name,'lon');
+SS=zeros(nx,ny,nt);
+TS=zeros(nx,ny,nt);
+US=zeros(nx,ny,nt);
+VS=zeros(nx,ny,nt);
+for t=1:nt
+    SS(:,:,t)=ncread([pnm fnm(t).name],'SS');
+    TS(:,:,t)=ncread([pnm fnm(t).name],'TS');
+    US(:,:,t)=ncread([pnm fnm(t).name],'US');
+    VS(:,:,t)=ncread([pnm fnm(t).name],'VS');
+end
