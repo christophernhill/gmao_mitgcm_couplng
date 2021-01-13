e6a5c618a74de513dd024307f8009335b623f926
menemenlis <dmenemenlis@gmail.com>
2021-01-12 18:48:28 -0800

    transferring bug fixes from c90_llc90_05 to c1440_llc2160_02 adopting SOLVE_DIAGONAL_LOWMEMORY as per January 7/8, 2021 discussion in [MITgcm-support] SOLVE_DIAGONAL_LOWMEMORY

--- experiments/c1440_llc2160_02/code/recvTask.c
+++ experiments/c1440_llc2160_02/code/recvTask.c
@@ -130,7 +130,7 @@ dataFieldDepth_t fieldDepths[] = {
 
    { 'E', 1 },
    { 'F', 1 },
-// { 'G', 1 },
+   { 'G', 1 },
    { 'H', 1 },
    { 'I', 1 },
    { 'J', 1 },
@@ -179,6 +179,7 @@ fieldInfoThisEpoch_t fieldsForEpochStyle_0[] = {
   { 'T', MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, 0, -1, NULL, 0, "Theta.%010d.%s", 0,0 },
   { 'N', MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, 0, -1, NULL, 0, "Eta.%010d.%s", 0,0 },
 
+  { 'A', MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, 0, -1, NULL, 0, "SItice.%010d.%s", 0,0 },
   { 'B', MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, 0, -1, NULL, 0, "SIarea.%010d.%s", 0,0 },
   { 'C', MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, 0, -1, NULL, 0, "SIheff.%010d.%s", 0,0 },
   { 'D', MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, 0, -1, NULL, 0, "SIhsnow.%010d.%s", 0,0 },
